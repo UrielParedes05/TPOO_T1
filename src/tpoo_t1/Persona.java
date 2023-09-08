5b39bc777efd4441b13d9b6567b771c9727eeede
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-09-08 13:24:14 -0500

    Add files via upload

---
+++ src/tpoo_t1/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpoo_t1;
+
+/**
+ *
+ * @author HP
+ */
+public class Persona {
+    
+    private String datos;
+    
+    public Persona(String datos) {
+        this.datos = datos;
+    }
+    
+    public Persona(){       
+    }
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+    
+}
---
+++ src/tpoo_t1/TPOO_T1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpoo_t1;
+
+/**
+ *
+ * @author HP
+ */
+public class TPOO_T1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PrincipalFrame PFrame = new PrincipalFrame();
+        
+        PFrame.setVisible(true);
+        PFrame.setLocationRelativeTo(null);
+    }
+    
+}
